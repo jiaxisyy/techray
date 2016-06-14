31918e10cb40d2a017fcf0ed4c2e5061c6289dc8
zuheng.lv <289019372@qq.com>
2016-06-14 14:47:04 +0800

    6-14

--- app/src/main/java/popupwindow/PopupForWeek.java
+++ app/src/main/java/popupwindow/PopupForWeek.java
@@ -4,14 +4,10 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
-import android.renderscript.ScriptGroup;
-import android.renderscript.Type;
-import android.text.InputType;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -26,14 +22,14 @@ import utils.ReadAndWrite;
 public class PopupForWeek implements View.OnClickListener {
     private PopupWindow popupWindow;
     private View contentView;
-    private EditText pup_et;
+    private Button monday,tuesday,wednesday,thursday,friday,saturday,sunday;
     private int type;
     private int[] address;
     private View view;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public PopupForWeek(Context context){
-        contentView = LayoutInflater.from(context).inflate(R.layout.pp_input, null);
+        contentView = LayoutInflater.from(context).inflate(R.layout.week_layout, null);
         popupWindow = new PopupWindow(contentView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,false);
         if(popupWindow.isShowing()){
             popupWindow.dismiss();
@@ -42,19 +38,32 @@ public class PopupForWeek implements View.OnClickListener {
         popupWindow.setOutsideTouchable(true);
         popupWindow.setBackgroundDrawable(new BitmapDrawable());
         popupWindow.setAnimationStyle(R.style.AnimationPreview);
-        pup_et = (EditText) contentView.findViewById(R.id.pup_et);
-        Button pup_comfirm = (Button)contentView.findViewById(R.id.pup_comfirm);
+        initView();
+    }
+    private void initView(){
+        monday = (Button)  contentView.findViewById(R.id.monday);
+        tuesday = (Button)  contentView.findViewById(R.id.tuesday);
+        wednesday = (Button)  contentView.findViewById(R.id.wednesday);
+        thursday = (Button)  contentView.findViewById(R.id.thursday);
+        friday = (Button)  contentView.findViewById(R.id.friday);
+        saturday = (Button)  contentView.findViewById(R.id.saturday);
+        sunday = (Button)  contentView.findViewById(R.id.sunday);
         TextView pup_cancel = (TextView)contentView.findViewById(R.id.pup_cancel);
-        pup_comfirm.setOnClickListener(this);
+        monday.setOnClickListener(this);
+        tuesday.setOnClickListener(this);
+        wednesday.setOnClickListener(this);
+        thursday.setOnClickListener(this);
+        friday.setOnClickListener(this);
+        saturday.setOnClickListener(this);
+        sunday.setOnClickListener(this);
         pup_cancel.setOnClickListener(this);
     }
+
     public void showPopupWindow( View view, int type, int[] address){
         this.view = view;
         this.type = type;
         this.address = address;
         if(!popupWindow.isShowing()){
-            pup_et.setText("");
-            pup_et.setInputType(InputType.TYPE_CLASS_TEXT);
             popupWindow.showAtLocation(contentView, Gravity.FILL,0,0);
         }
     }
@@ -68,38 +77,32 @@ public class PopupForWeek implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.pup_comfirm:
-                String[] input ;
-                if(pup_et.getText().toString()!=null && !pup_et.getText().toString().equals("")){
-                    if(pup_et.getText().toString().equals("一") || pup_et.getText().toString()=="1"|| pup_et.getText().toString().equals("1")){
-                         input = new String[]{"1"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("二") || pup_et.getText().toString()=="2"|| pup_et.getText().toString().equals("2")){
-                        input = new String[]{"2"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("三") || pup_et.getText().toString()=="3"|| pup_et.getText().toString().equals("3")){
-                        input = new String[]{"3"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("四") || pup_et.getText().toString()=="4"|| pup_et.getText().toString().equals("4")){
-                        input = new String[]{"4"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("五") || pup_et.getText().toString()=="5"|| pup_et.getText().toString().equals("5")){
-                        input = new String[]{"5"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("六") || pup_et.getText().toString()=="6"|| pup_et.getText().toString().equals("6")){
-                        input = new String[]{"6"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                    if(pup_et.getText().toString().equals("天") || pup_et.getText().toString()=="7"|| pup_et.getText().toString().equals("7")){
-                        input = new String[]{"0"};
-                        new ReadAndWrite().WriteJni(type,address,input);
-                    }
-                }
+            case R.id.monday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"1"});
+                popupWindow.dismiss();
+                break;
+            case R.id.tuesday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"2"});
+                popupWindow.dismiss();
+                break;
+            case R.id.wednesday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"3"});
+                popupWindow.dismiss();
+                break;
+            case R.id.thursday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"4"});
+                popupWindow.dismiss();
+                break;
+            case R.id.friday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"5"});
+                popupWindow.dismiss();
+                break;
+            case R.id.saturday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"6"});
+                popupWindow.dismiss();
+                break;
+            case R.id.sunday:
+                new ReadAndWrite().WriteJni(type,address,new String[]{"0"});
                 popupWindow.dismiss();
                 break;
             case R.id.pup_cancel:
