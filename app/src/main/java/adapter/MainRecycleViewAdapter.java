4c499565fb76014dfc2481129d035e478d8fe4a9
shuang.xiang <xyzsyy0102@163.com>
2016-06-29 11:18:57 +0800

    空指针异常处理

--- app/src/main/java/adapter/MainRecycleViewAdapter.java
+++ app/src/main/java/adapter/MainRecycleViewAdapter.java
@@ -39,7 +39,7 @@ public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleView
                 case 1:
                     for (int i = 0; i <= list.size() - 1; i++) {
 
-                        if (msg.getData().get(list.get(i).getStr()).toString() != null) {
+                        if (msg.getData().getString(list.get(i).getStr())!= null) {
                             float v = Float.parseFloat(msg.getData().get(list.get(i).getStr()).toString());
                             list.get(i).setData(String.valueOf((float) Math.round(v * 100) / 100));
                         }
